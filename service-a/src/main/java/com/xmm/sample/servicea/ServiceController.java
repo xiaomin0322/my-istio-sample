44d5ac6a1218997ae8a8da2e9bcc217e7974a5ad
zhangzengmin <xiaomin0322@sina.com>
2019-09-10 11:32:04 +0800

    update

--- service-a/src/main/java/com/xmm/sample/servicea/ServiceController.java
+++ service-a/src/main/java/com/xmm/sample/servicea/ServiceController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
@@ -25,12 +27,27 @@ public class ServiceController {
 
     @Value("${app.version}")
     private String version;
+    
+	// 全局的trace对象
+	@Autowired
+	Tracer tracer;
 
     @GetMapping("/info")
     public String info() {
+    	System.out.println("tracer:"+tracer);
+    	addSpan();
         String rsp = "A Service version = " + this.version + "  ===> " + notify(url);
         log.info(rsp);
         return rsp;
+    }
+    
+    public void addSpan() {
+    	Span currentSpan = tracer.getCurrentSpan();
+		if (currentSpan == null) {
+			return;
+		}
+		currentSpan.tag("http.method", "zzm");
+		currentSpan.tag("pod.name", "asdasd");
     }
 
     public String notify(String url) {
